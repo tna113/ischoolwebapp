52465b7dd67de24d481aa72f438817693cc5060f
Thea Arias <99890039+tna113@users.noreply.github.com>
2022-05-17 18:44:32 -0400

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+//undergrad & grad jquery tabs
+$(function () {
+    $("#tabs").tabs();
+});
+
+//minors
+$(function () {
+    $("#accordion").accordion();
+});
